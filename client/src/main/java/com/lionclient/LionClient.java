3f49e619602e07538dea9703f7df8a46d432b433
LionClient <144057425+LionClientINC@users.noreply.github.com>
2026-05-25 15:02:38 +0200

    version fix

--- client/src/main/java/com/lionclient/LionClient.java
+++ client/src/main/java/com/lionclient/LionClient.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 public final class LionClient {
     public static final String MOD_ID  = "lionclientinjectable";
     public static final String NAME    = "LionClientInjectable";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     static {
         registerFakeModContainer();
