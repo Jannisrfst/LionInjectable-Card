6f0a5ddfb3226f32368a338cdb0cddc9312a11eb
LionClient <144057425+LionClientINC@users.noreply.github.com>
2026-05-25 16:34:54 +0200

    Fix Badlion and Lunar compatability

--- client/src/main/java/com/lionclient/LionClient.java
+++ client/src/main/java/com/lionclient/LionClient.java
@@ -21,7 +21,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 public final class LionClient {
     public static final String MOD_ID  = "lionclientinjectable";
     public static final String NAME    = "LionClientInjectable";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     static {
         registerFakeModContainer();
